de98341010c8c9ea0078b69411faaffe62cfad27
syedgulzar786 <syedgulzarmohammad123@gmail.com>
2024-06-09 05:44:11 +0500

    a logic applying in chatbot

---
+++ app.js
@@ -0,0 +1,22 @@
+// var inputBox = document.getElementById("input-box")
+// var store = "";
+// var show = "";
+// var swiper = "";
+// function addEnter(event){
+//  if(event.keyCode == 13){    
+//     store = inputBox.value;
+//     var checkin = ["hn","better",'goo','amazing','gre','th','maze','me','al','hum'];
+//     // console.log(store)
+// for(var i = 0; i < checkin.length; i++){
+//     swiper = checkin[i]
+//     show = store.indexOf(swiper)
+//     if(show !== -1){    
+// break;
+//     }
+// }
+// if(show !== -1){    
+//     console.log(show);
+// }
+// inputBox.value = "";
+// }
+// }
--- script.js
+++ script.js
@@ -47,3 +47,4 @@ function toDelete(event){
 function deleteAll(){
     listContainer.innerHTML = "";
 }
+
